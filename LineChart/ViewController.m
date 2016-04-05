7766311beaee5a4e0ea6bb22310e7e0c4cb88f17
yangyong <yangyongios@163.com>
2016-04-05 11:09:44 +0800

    change data

--- LineChart/ViewController.m
+++ LineChart/ViewController.m
@@ -21,13 +21,22 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = [UIColor whiteColor];
+   
+    [self initLineChartView];
+    
+  
+    
+}
+
+- (void)initLineChartView
+{
     NSMutableArray* xArray = [[NSMutableArray alloc] initWithCapacity:7];
     
     for (int i = 7; i > 0; i--) {
         NSDate *  senddate=[NSDate date];
         
         NSDateFormatter  *dateformatter=[[NSDateFormatter alloc] init];
-          [dateformatter setDateFormat:kDATE_FORMAT_Str];
+        [dateformatter setDateFormat:kDATE_FORMAT_Str];
         NSTimeInterval timeStart = 24 * 3600 * i;
         
         NSDate * oldData = [senddate dateByAddingTimeInterval:-timeStart];
@@ -36,12 +45,12 @@
     }
     
     NSMutableArray* yArray = [[NSMutableArray alloc]init];
-//    NSMutableDictionary* dict = [[NSMutableDictionary alloc] initWithObjects:@[@[@"176",@"177",@"180",@"190"],@"身高"] forKeys:@[@"array",@"title"]];
-  
-     NSMutableDictionary* newDict = [[NSMutableDictionary alloc] initWithObjects:@[@[@"7.4",@"6.6",@"6.22",@"6.2240",@"6.720",@"7.43",@"6.77777"],@"年化"] forKeys:@[kDATAARRAY_KEY,@"title"]];
+    //    NSMutableDictionary* dict = [[NSMutableDictionary alloc] initWithObjects:@[@[@"176",@"177",@"180",@"190"],@"身高"] forKeys:@[@"array",@"title"]];
+    
+    NSMutableDictionary* newDict = [[NSMutableDictionary alloc] initWithObjects:@[@[@"7.4",@"6.6",@"6.22",@"6.2240",@"6.720",@"7.43",@"6.77777"],@"年化"] forKeys:@[kDATAARRAY_KEY,@"title"]];
     
     
-//    [yArray addObject:dict];
+    //    [yArray addObject:dict];
     [yArray addObject:newDict];
     
     NSDate *  senddate=[NSDate date];
@@ -52,7 +61,7 @@
     
     NSString *  locationString=[dateformatter stringFromDate:senddate];
     
-   
+    
     NSTimeInterval timeStart = 24 * 3600 * 7;
     
     NSDate * oldData = [senddate dateByAddingTimeInterval:-timeStart];
@@ -63,13 +72,10 @@
     NSDate * lastData = [senddate dateByAddingTimeInterval:-timeEnd];
     NSString * endDate = [dateformatter stringFromDate:lastData];
     
-     NSLog(@"locationString:%@  start %@  end %@",locationString,startDate,endDate);
+    NSLog(@"locationString:%@  start %@  end %@",locationString,startDate,endDate);
     LCLineChartView* chartView = [ZCLineChart drawChartViewBeginTime:startDate EndTime:endDate Rect:CGRectMake(25, 60, self.view.frame.size.width - 50, 200) Unit:@"%" XArray:xArray YArray:yArray];
     
     [self.view addSubview:chartView];
-
-  
-    
 }
 
 - (void)didReceiveMemoryWarning {
